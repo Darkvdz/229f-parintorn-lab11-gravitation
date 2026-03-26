eecf4fef66de7f146fa49a2bb7835ab4906edf5a
Darkvdz <famparintorn@gmail.com>
2026-03-26 10:33:52 +0700

    Gravity_Script

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -1,16 +1,48 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static List<Gravity> otherObj;
+    Rigidbody rb;
+    const float G = 0.006674f; //Gravitational Constant 6.674
+   
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (otherObj == null)
+        {
+            otherObj = new List<Gravity>();
+        }
+        otherObj.Add(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
+        foreach (Gravity obj in otherObj)
+        {
+            if (obj != this) //กันไม่ให้โดนแรงดึงดูดของตัวเอง
+            {
+                AttractForce(obj);
+            }
+        }
+    }
+
+    void AttractForce(Gravity other)
+    {
+        Rigidbody otherRb = other.rb; // ดึงค่ามวล m
+        Vector3 direction = rb.position - otherRb.position; // ทิศทางจากวัตถุ M ไป m
+
+        float distance = direction.magnitude; // หาระยะห่าง r
+        if (distance == 0f) return; // กันไม่ให้มีแรงดึงดูดเมื่อวัตถุอยู่ตำแหน่งเดียวกัน
+
+        //F = G(m1 * m2) /r^2
+        float forceMagnitude = G * (rb.mass - otherRb.mass) / Mathf.Pow(distance, 2);
+        Vector3 gravitationalForce = forceMagnitude * direction.normalized; //นำแรงที่ได้มาใส่ทิศทาง 1,-1
+        otherRb.AddForce(gravitationalForce); //ใส่แรงดึงดูดพร้อมทิศทางให้กับวัตถุ
+
     }
 }
